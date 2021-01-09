b505da016106d903fec272a9c1e7d545cae5a269
RaviKanth-31 <74865487+RaviKanth-31@users.noreply.github.com>
2021-01-09 08:16:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1 = Math.random();
 randomNumber1= Math.floor(randomNumber1*6 +1);
-var randomImageSource1="images/dice"+randomNumber1+".png"
+var randomImageSource1="dice"+randomNumber1+".png"
 
 var randomNumber2 = Math.random();
 randomNumber2 = Math.floor(randomNumber2*6 +1);
-var randomImageSource2="images/dice"+randomNumber2+".png";
+var randomImageSource2="dice"+randomNumber2+".png";
 
 document.querySelector(".img1").setAttribute("src",randomImageSource1);
 document.querySelector(".img2").setAttribute("src",randomImageSource2);
